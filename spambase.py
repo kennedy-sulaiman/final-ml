980c998a210351e7c21589dc6db90380dbb2c819
Kennedy <kennedy@asus.localdomain>
2019-12-10 15:28:50 +0800

    final machine learning

---
+++ spambase.py
@@ -0,0 +1,13 @@
+#%%
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import seaborn as sns
+import warnings
+import time
+from collections import Counter
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
+from scipy import stats
